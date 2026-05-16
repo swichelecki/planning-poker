800b2ad49dac5cf31d6ed5d6898c7659ca3e2aee
Steve Wichelecki <wichels@shure.com>
2026-05-16 12:38:27 -0500

    Added eslint and removed try / catch from page server component

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -2,27 +2,22 @@ import { Suspense } from 'react';
 import connectDB from '../../../config/db';
 import User from '../../../models/User';
 import { Login } from '../../../components';
-import { getUserFromCookie, handleServerError } from '../../../utilities';
+import { getUserFromCookie } from '../../../utilities';
 
 export const metadata = {
   title: 'Log In',
 };
 
 async function LoginWithData() {
-  try {
-    const { userId } = await getUserFromCookie();
+  const { userId } = await getUserFromCookie();
 
-    if (!userId) return <Login user={{}} />;
+  if (!userId) return <Login user={{}} />;
 
-    await connectDB();
-    const userRaw = await User.findOne({ _id: userId });
-    const user = JSON.parse(JSON.stringify(userRaw));
+  await connectDB();
+  const userRaw = await User.findOne({ _id: userId });
+  const user = JSON.parse(JSON.stringify(userRaw));
 
-    return <Login user={user ?? {}} />;
-  } catch (error) {
-    const errorMessage = handleServerError(error);
-    console.error(errorMessage);
-  }
+  return <Login user={user ?? {}} />;
 }
 
 export default function LoginPage() {
---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import nextCoreWebVitals from 'eslint-config-next/core-web-vitals';
+
+const eslintConfig = [
+  ...nextCoreWebVitals,
+  {
+    rules: {
+      'react-hooks/exhaustive-deps': 'off',
+    },
+  },
+  {
+    ignores: [
+      'node_modules/**',
+      '.next/**',
+      'out/**',
+      'build/**',
+      'next-env.d.ts',
+    ],
+  },
+];
+
+export default eslintConfig;
